12f83e58be19438900ef80bc47450e659967aebc
ColinTravis <colint@cybertravis.us>
2019-02-11 19:09:43 -0600

    Added basic sorting

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Colle McVoy Coding Challenge
 
 Details:
-- Fetch GitHub trending repositories from the following URL — https://github-trending-api.now.sh/repositories
-- Display the results on the page including the repo name, description, and stars count
+- ~~Fetch GitHub trending repositories from the following URL — https://github-trending-api.now.sh/repositories~~
+- ~~Display the results on the page including the repo name, description, and stars count~~
 - Add the ability to sort by stars count (most to least)
 - Feel free to use any frameworks of choice (or just use vanilla JavaScript)
 - You should only use HTML, CSS, and JavaScript
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,14 @@
     <section>
         <div class="">
             <ul class="">
-                <li v-for="(repo, key) in repos" :key="key">
-                    <h1>{{repo.name}}</h1>
+                <a v-on:click="sortOrder = 'A-Z'" >A-Z</a><br>
+                <a v-on:click="sortOrder = 'Z-A'" >Z-A</a><br>
+                <a v-on:click="sortOrder = 'starsAsc'" >Stars ^</a><br>
+                <a v-on:click="sortOrder = 'starsDesc'" >Stars down</a>
+                <li v-for="(repo, key) in sort(sortOrder)" :key="key">
+                    <h1 style="text-transform: capitalize;">{{repo.name}}</h1>
                     <p>{{repo.author}}</p>
+                    <p>Stars: {{repo.stars}}</p>
                 </li>
             </ul>
         </div>
@@ -16,9 +21,44 @@
         async asyncData({app}) {
             const {data} = await app.$axios.get("https://github-trending-api.now.sh/repositories")
 
-            return {repos: data}
+            return {
+                repos: data,
+                sortOrder: "A-Z"
+            }
+        },
+        computed: {
+            orderedListOptions: function () {
+                return {
+                    "A-Z": () => {
+                        return this.repos.slice().sort().reverse()
+                    },
+                    "Z-A": () => {
+                        return this.repos.slice().sort()
+                    },
+                    "starsAsc": () => {
+                        return this.repos.slice().sort((a,b) => {return b.stars - a.stars});
+                    },
+                    "starsDesc": () => {
+                        return this.repos.slice().sort((a,b) => {return b.stars - a.stars}).reverse();
+                    },
+                }
+            },
+        },
+        methods: {
+            sort: function (sortOrder) {
+                return this.orderedListOptions[sortOrder]()
+            },
+        },
+        head () {
+            return {
+                title: "Repos",
+                meta: [
+                    { hid: 'description', name: 'description' },
+                ]
+            }
         }
     }
+
 </script>
 
 <style>
